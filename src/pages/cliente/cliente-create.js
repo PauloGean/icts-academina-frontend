a7b94764dd4341c5054e41e3f56bdd608055e8b0
Paulo Lopes <paulogean2@gmail.com>
2022-10-15 13:53:23 -0400

    crud cliente

--- src/pages/cliente/cliente-create.js
+++ src/pages/cliente/cliente-create.js
@@ -2,46 +2,100 @@
 import '../../MeuCss.css'
 
 import api from '../../api';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Titulo from '../../components/titulo';
 
-function ClienteCreate() {
-    const [nome,setNome] = useState("");
-    const [cpf,setCpf] = useState("");
+function ClienteCreate(props) {
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [novo, setNovo] = useState(true)
+    const [id, setId] = useState(null);
+    const [showView, setShowView] = useState(false)
 
-    function mudouNome(event){
+    useEffect(() => {
+        if (props.cliente != null) {
+            setNome(props.cliente.nome)
+            setCpf(props.cliente.cpf)
+            setId(props.cliente.idcliente)
+            setNovo(false)
+        } else {
+            setNome('')
+            setCpf('')
+            setId(null)
+            setNovo(true)
+        }
+    }, [props.cliente])
+
+    useEffect(() => {
+        setShowView(props.show)
+
+    }, [props.show])
+
+
+    function mudouNome(event) {
         setNome(event.target.value)
     }
 
-    function salvar(){
-        var dados={"nome":nome, "cpf":cpf}
+    function salvar() {
+        var dados = { "nome": nome, "cpf": cpf }
         console.log(dados);
 
-        api.post('cliente', dados).then(e=>{
+        api.post('cliente', dados).then(e => {
             setNome("");
             setCpf("");
-            alert("Salvo com sucesso");
+            props.refresh()
+            // alert("Salvo com sucesso");
+        })
+
+    }
+
+
+    function atualizar() {
+        var dados = { "nome": nome, "cpf": cpf }
+        console.log(dados);
+
+        api.put('cliente/' + id, dados).then(e => {
+            setNome("");
+            setCpf("");
+            setId(null)
+            props.refresh()
+            // alert("Salvo com sucesso");
         })
 
     }
 
 
     return (
-        <div >
-            <h1 className='meuTitulo'>Criar Cliente</h1>
-            <form>
-                <label>
-                    Nome:
-                    <input type="text" value={nome}  onChange={mudouNome} name="nome" />
-                </label>
-                <label>
-                    CPF:
-                    <input type="text" name="cpf" value={cpf} 
-                     onChange={(e)=>setCpf(e.target.value)}/>
-                </label>
-                <button type='button' onClick={salvar} >Salvar</button>
-            </form>
+        <>
+            {showView==true ?
+                <div >
+                    {novo ? <Titulo valor="Cadastrar Cliente" /> : <Titulo valor="Atualizar Cliente" />}
 
-        </div>
+                    <form>
+                        <label>
+                            Nome:
+                            <input type="text" value={nome} onChange={mudouNome} name="nome" />
+                        </label>
+                        <label>
+                            CPF:
+                            <input type="text" name="cpf" value={cpf}
+                                onChange={(e) => setCpf(e.target.value)} />
+                        </label>
+                        {/* {someItem ? displayThisIfTrue : displayThisIfFalse} */}
+                        {novo ?
+                            <button type='button' onClick={salvar} >Salvar</button>
+                            :
+                            <button type='button' onClick={atualizar} >Atualizar</button>
+                        }
+
+
+
+
+                    </form>
+
+                </div>:""}
+
+        </>
     );
 }
 
